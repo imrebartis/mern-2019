ece5d03bf6e0fa93ac09c1b1d35c44b47a9ea592
Imre Bartis <imre.bartis@siili.com>
2019-08-14 17:37:21 +0300

    implement jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -57,10 +59,23 @@ router.post(
 
       // save user in db
       await user.save();
-  
-      // TODO return jsonwebtoken
 
-      res.send('user registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+  
+      // return jsonwebtoken
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
